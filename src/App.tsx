2fceda33a5c77d2c6844560ef00ff914db6df33e
Volodymyr Klymiuk <volodymyr.klymiuk@jetsoftpro.com>
2023-05-10 03:14:49 +0300

    'done'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable import/order */
 import React from 'react';
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Navbar } from './components/navbar/Navbar';
 import { Home } from './pages/Home/Home';
 import { About } from './pages/About/About';
@@ -13,12 +14,18 @@ const App: React.FC = () => (
 
     <div className="pages">
       <Routes>
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
         <Route path="/" Component={Home} />
         <Route path="/about" Component={About} />
         <Route path="/blog" Component={Blog} />
         <Route path="#footer" Component={Footer} />
+
       </Routes>
     </div>
+
   </>
 );
 
